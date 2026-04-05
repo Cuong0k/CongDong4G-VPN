40c132082dab4f0a634c93e6a2bcdda15c3650bd
kenno <154305973+Cuong0k@users.noreply.github.com>
2026-04-05 10:26:19 +0700

    add: src/constants/config.ts

---
+++ src/constants/config.ts
@@ -0,0 +1,9 @@
+export const API_BASE_URL = 'https://panel.befast.cfd/api';
+
+export const APP_CONFIG = {
+  appName: 'CongDong4G VPN',
+  version: '1.0.0',
+  website: 'https://congdong4g.com',
+  zalo: 'https://zalo.me/g/congdong4g',
+  support_email: 'support@congdong4g.com',
+};
